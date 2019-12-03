b1bb0a466d1efb9394c9df6f3ee57bc8d33451a8
elementrix <sgh04069@naver.com>
2019-12-03 23:01:21 +0900

    functional things

--- app/src/main/java/com/e/moal_admin/NotificationFragment.kt
+++ app/src/main/java/com/e/moal_admin/NotificationFragment.kt
@@ -2,6 +2,7 @@ package com.e.moal_admin
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +20,8 @@ class NotificationFragment : Fragment() {
 
     var rootRef: DatabaseReference = FirebaseDatabase.getInstance().getReference()
     val dirFire: DatabaseReference = rootRef.child("stores").child("노랑통닭 홍대점") // **수정필요**
+    var personLIst : MutableList<PositionApplyPerson> = ArrayList()
+    val positionPartNameSet : MutableList<MutableList<String>> = ArrayList() // [[목, 서빙, 마감],[목, 서빙, 오픈],...]
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,8 +32,6 @@ class NotificationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val positionPartNameSet : MutableList<MutableList<String>> = ArrayList() // [[목, 서빙, 마감],[목, 서빙, 오픈],...]
 
         dirFire.child("WorkingPart").addValueEventListener(object: ValueEventListener {
             override fun onDataChange(p0: DataSnapshot) {
@@ -54,204 +55,85 @@ class NotificationFragment : Fragment() {
             }
         })
 
-        var personLIst : MutableList<PositionApplyPerson> = ArrayList()
-
-        mon.setOnClickListener(){
+        mon.setOnClickListener(View.OnClickListener{
             mon.setBackgroundColor(Color.rgb(250, 171, 26))
 
             personLIst.clear()//시작하기전에 비워줘야 누적 안됨 액션에 따라 바뀌므로 액션 시작에 넣어줌
 
-            for(k in positionPartNameSet){
-                if(k[0].equals("월")){
-                    dirFire.child("WorkingPart").child("월").child(k[1]).child(k[2]).child("RequestList").addValueEventListener(object: ValueEventListener {
-                        override fun onDataChange(p0: DataSnapshot) {
-                            for (j in p0.children){
-                                val person = PositionApplyPerson(k[1],k[2],j.key!!) // 얘도 마찬가지 무조건 이름을 등록하게 되어있음 절대 null이 불가능 그러니 제발 믿어줘,,,
-                                personLIst.add(person)
-                            }
-                        }
-                        override fun onCancelled(p0: DatabaseError) {
-
-                        }
-                    })
-                }
-            }
+            fillPersonList("월")
 
             position_list.apply {
                 layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL, false)
                 adapter = PositionListAdapter(personLIst)
             }
-            worker_list.apply {
-                layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL, false)
-                adapter = WorkerListAdapter(personLIst)
-            }
-        }
-        tue.setOnClickListener(){
+        })
+
+        tue.setOnClickListener(View.OnClickListener{
 
             tue.setBackgroundColor(Color.rgb(250, 171, 26))
             personLIst.clear()//시작하기전에 비워줘야 누적 안됨 액션에 따라 바뀌므로 액션 시작에 넣어줌
 
-            for(k in positionPartNameSet){
-                if(k[0].equals("화")){
-                    dirFire.child("WorkingPart").child("화").child(k[1]).child(k[2]).child("RequestList").addValueEventListener(object: ValueEventListener {
-                        override fun onDataChange(p0: DataSnapshot) {
-                            for (j in p0.children){
-                                val person = PositionApplyPerson(k[1],k[2],j.key!!) // 얘도 마찬가지 무조건 이름을 등록하게 되어있음 절대 null이 불가능 그러니 제발 믿어줘,,,
-                                personLIst.add(person)
-                            }
-                        }
-                        override fun onCancelled(p0: DatabaseError) {
-
-                        }
-                    })
-                }
-            }
+            fillPersonList("화")
 
             position_list.apply {
                 layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL, false)
                 adapter = PositionListAdapter(personLIst)
             }
-            worker_list.apply {
-                layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL, false)
-                adapter = WorkerListAdapter(personLIst)
-            }
 
-        }
-        wed.setOnClickListener(){
+        })
+
+        wed.setOnClickListener(View.OnClickListener{
             wed.setBackgroundColor(Color.rgb(250, 171, 26))
             personLIst.clear()//시작하기전에 비워줘야 누적 안됨 액션에 따라 바뀌므로 액션 시작에 넣어줌
 
-            for(k in positionPartNameSet){
-                if(k[0].equals("수")){
-                    dirFire.child("WorkingPart").child("수").child(k[1]).child(k[2]).child("RequestList").addValueEventListener(object: ValueEventListener {
-                        override fun onDataChange(p0: DataSnapshot) {
-                            for (j in p0.children){
-                                val person = PositionApplyPerson(k[1],k[2],j.key!!) // 얘도 마찬가지 무조건 이름을 등록하게 되어있음 절대 null이 불가능 그러니 제발 믿어줘,,,
-                                personLIst.add(person)
-                            }
-                        }
-                        override fun onCancelled(p0: DatabaseError) {
-
-                        }
-                    })
-                }
-            }
+            fillPersonList("수")
 
             position_list.apply {
                 layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL, false)
                 adapter = PositionListAdapter(personLIst)
             }
-            worker_list.apply {
-                layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL, false)
-                adapter = WorkerListAdapter(personLIst)
-            }
 
-        }
-        thr.setOnClickListener(){
+        })
+        thr.setOnClickListener(View.OnClickListener{
             thr.setBackgroundColor(Color.rgb(250, 171, 26))
             personLIst.clear()//시작하기전에 비워줘야 누적 안됨 액션에 따라 바뀌므로 액션 시작에 넣어줌
 
-            for(k in positionPartNameSet){
-                if(k[0].equals("목")){
-                    dirFire.child("WorkingPart").child("목").child(k[1]).child(k[2]).child("RequestList").addValueEventListener(object: ValueEventListener {
-                        override fun onDataChange(p0: DataSnapshot) {
-                            for (j in p0.children){
-                                val person = PositionApplyPerson(k[1],k[2],j.key!!) // 얘도 마찬가지 무조건 이름을 등록하게 되어있음 절대 null이 불가능 그러니 제발 믿어줘,,,
-                                personLIst.add(person)
-                            }
-                        }
-                        override fun onCancelled(p0: DatabaseError) {
+            fillPersonList("목")
 
-                        }
-                    })
-                }
-            }
             position_list.apply {
                 layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL, false)
                 adapter = PositionListAdapter(personLIst)
             }
-            worker_list.apply {
-                layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL, false)
-                adapter = WorkerListAdapter(personLIst)
-            }
-        }
-        fri.setOnClickListener(){
+
+        })
+        fri.setOnClickListener(View.OnClickListener{
             fri.setBackgroundColor(Color.rgb(250, 171, 26))
             personLIst.clear()//시작하기전에 비워줘야 누적 안됨 액션에 따라 바뀌므로 액션 시작에 넣어줌
 
-            for(k in positionPartNameSet){
-                if(k[0].equals("금")){
-                    dirFire.child("WorkingPart").child("금").child(k[1]).child(k[2]).child("RequestList").addValueEventListener(object: ValueEventListener {
-                        override fun onDataChange(p0: DataSnapshot) {
-                            for (j in p0.children){
-                                val person = PositionApplyPerson(k[1],k[2],j.key!!) // 얘도 마찬가지 무조건 이름을 등록하게 되어있음 절대 null이 불가능 그러니 제발 믿어줘,,,
-                                personLIst.add(person)
-                            }
-                        }
-                        override fun onCancelled(p0: DatabaseError) {
-
-                        }
-                    })
-                }
-            }
+            fillPersonList("금")
 
             position_list.apply {
                 layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL, false)
                 adapter = PositionListAdapter(personLIst)
             }
-            worker_list.apply {
-                layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL, false)
-                adapter = WorkerListAdapter(personLIst)
-            }
-        }
-        sat.setOnClickListener(){
+        })
+        sat.setOnClickListener(View.OnClickListener{
             sat.setBackgroundColor(Color.rgb(250, 171, 26))
             personLIst.clear()//시작하기전에 비워줘야 누적 안됨 액션에 따라 바뀌므로 액션 시작에 넣어줌
 
-            for(k in positionPartNameSet){
-                if(k[0].equals("토")){
-                    dirFire.child("WorkingPart").child("토").child(k[1]).child(k[2]).child("RequestList").addValueEventListener(object: ValueEventListener {
-                        override fun onDataChange(p0: DataSnapshot) {
-                            for (j in p0.children){
-                                val person = PositionApplyPerson(k[1],k[2],j.key!!) // 얘도 마찬가지 무조건 이름을 등록하게 되어있음 절대 null이 불가능 그러니 제발 믿어줘,,,
-                                personLIst.add(person)
-                            }
-                        }
-                        override fun onCancelled(p0: DatabaseError) {
-
-                        }
-                    })
-                }
-            }
+            fillPersonList("토")
 
             position_list.apply {
                 layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL, false)
                 adapter = PositionListAdapter(personLIst)
             }
-            worker_list.apply {
-                layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL, false)
-                adapter = WorkerListAdapter(personLIst)
-            }
-        }
-        sun.setOnClickListener(){
+        })
+        sun.setOnClickListener(View.OnClickListener{
             sun.setBackgroundColor(Color.rgb(250, 171, 26))
             personLIst.clear()//시작하기전에 비워줘야 누적 안됨 액션에 따라 바뀌므로 액션 시작에 넣어줌
 
-            for(k in positionPartNameSet){
-                if(k[0].equals("일")){
-                    dirFire.child("WorkingPart").child("일").child(k[1]).child(k[2]).child("RequestList").addValueEventListener(object: ValueEventListener {
-                        override fun onDataChange(p0: DataSnapshot) {
-                            for (j in p0.children){
-                                val person = PositionApplyPerson(k[1],k[2],j.key!!) // 얘도 마찬가지 무조건 이름을 등록하게 되어있음 절대 null이 불가능 그러니 제발 믿어줘,,,
-                                personLIst.add(person)
-                            }
-                        }
-                        override fun onCancelled(p0: DatabaseError) {
-
-                        }
-                    })
-                }
-            }
+            fillPersonList("일")
+            
             position_list.apply {
                 layoutManager = LinearLayoutManager(activity,LinearLayoutManager.HORIZONTAL, false)
                 adapter = PositionListAdapter(personLIst)
@@ -261,11 +143,28 @@ class NotificationFragment : Fragment() {
                 adapter = WorkerListAdapter(personLIst)
             }
 
-        }
+        })
         //리스너 등록 끝
         //어뎁터에 넣어서 리사이클러 뷰 채워줘야 함
 
+    }
+    fun fillPersonList(day : String){
+        val day = day
+        for(k in positionPartNameSet){
+            if(k[0].equals(day)){
+                dirFire.child("WorkingPart").child(day).child(k[1]).child(k[2]).child("RequestList").addValueEventListener(object: ValueEventListener {
+                    override fun onDataChange(p0: DataSnapshot) {
+                        for (j in p0.children){
+                            val person = PositionApplyPerson(k[1],k[2],j.key!!) // 얘도 마찬가지 무조건 이름을 등록하게 되어있음 절대 null이 불가능 그러니 제발 믿어줘,,,
+                            personLIst.add(person)
+                        }
+                    }
+                    override fun onCancelled(p0: DatabaseError) {
 
+                    }
+                })
+            }
+        }
     }
 
 }
